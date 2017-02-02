3c5d3b9d260a358f89adea0c019cdb823a84eab6
karlkaebnick <karl.kaebick@gmail.com>
2017-02-02 16:38:03 -0500

    Starting to flesh out the admin page

--- waste_wizard/admin.py
+++ waste_wizard/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import WasteItem
+
+
+class WasteItemAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['description', 'destination', 'notes', 'keywords']}),
+        # ('Destination', {'fields': ['destination'], 'classes': ['collapse']}),
+    ]
+    # inlines = [ChoiceInline]
+    list_display = ('description', 'destination', 'notes', 'keywords')
+    list_filter = ['description']
+    search_fields = ['description', 'keywords']
+
+admin.site.register(WasteItem, WasteItemAdmin)
