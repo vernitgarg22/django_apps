1ab873a78ae83408a1e7fa5d5dbf6986c1c4ab6e
karlkaebnick <karl.kaebnick@gmail.com>
2017-02-06 15:59:04 -0500

    Add more functionality to results page

--- waste_wizard/forms.py
+++ waste_wizard/forms.py
@@ -3,3 +3,6 @@ from django import forms
 
 class WasteItemSearchForm(forms.Form):
     description = forms.CharField(label='Describe the waste item', max_length=100)
+
+class WasteItemResultsForm(forms.Form):
+    description = forms.CharField(label='Describe another waste item', max_length=100)
--- waste_wizard/urls.py
+++ waste_wizard/urls.py
@@ -6,9 +6,8 @@ from . import views
 app_name = 'waste_wizard'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^results/$', views.results, name='results'),
-    url(r'^results/([0-9]{4})/$', views.results, name='results'),
-    url(r'^results/([ %0-9a-zA-Z]*)/$', views.results, name='results'),
-    # url(r'^results/(?P<pk>[a-zA-Z]+)/$', views.results, name='results'),
-    # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^results/$', views.ResultsView.as_view(), name='results'),
+    # url(r'^results/$', views.results, name='results'),
+    url(r'^results/([ %0-9a-zA-Z]*)/$', views.ResultsView.as_view(), name='results'),
+    # url(r'^results/([ %0-9a-zA-Z]*)/$', views.results, name='results'),
 ]
