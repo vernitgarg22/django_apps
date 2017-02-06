1113e1579679185939e02a8b1797d1b3b052fe87
karlkaebnick <karl.kaebnick@gmail.com>
2017-02-06 16:00:49 -0500

    Allow commas in search strings

--- waste_wizard/urls.py
+++ waste_wizard/urls.py
@@ -7,7 +7,5 @@ app_name = 'waste_wizard'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^results/$', views.ResultsView.as_view(), name='results'),
-    # url(r'^results/$', views.results, name='results'),
-    url(r'^results/([ %0-9a-zA-Z]*)/$', views.ResultsView.as_view(), name='results'),
-    # url(r'^results/([ %0-9a-zA-Z]*)/$', views.results, name='results'),
+    url(r'^results/([ %0-9a-zA-Z\,]*)/$', views.ResultsView.as_view(), name='results'),
 ]
