c44e10e544655c28d78a4fb8f5cb858104eb92db
karlkaebnick <karl.kaebick@gmail.com>
2017-02-14 13:54:58 -0500

    add latest migration

---
+++ waste_wizard/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('waste_wizard', '0003_auto_20170202_1659'),
+        ('waste_wizard', '0003_auto_20170208_1201'),
+    ]
+
+    operations = [
+    ]
