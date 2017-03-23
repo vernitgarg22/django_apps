e1a9e9caadf7aac19fc3b5e08c5608d352f51c39
karlkaebnick <karl.kaebick@gmail.com>
2017-03-23 11:50:17 -0400

    Waste Scheduler:  add admin class for waste schedule detail

--- waste_schedule/admin.py
+++ waste_schedule/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.forms import Textarea
 
-from .models import ScheduleChange
+from .models import ScheduleChange, ScheduleDetail
 
 
 class ScheduleChangeAdmin(admin.ModelAdmin):
@@ -14,4 +15,23 @@ class ScheduleChangeAdmin(admin.ModelAdmin):
     ordering = ['normal_day', 'service_type']
 
 
+class ScheduleDetailAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['detail_type', 'service_type', 'description', 'normal_day', 'new_day', 'note', 'waste_area_ids']}),
+    ]
+    list_display = ('detail_type', 'service_type', 'description', 'normal_day', 'new_day', 'note', 'waste_area_ids')
+    list_filter = ['detail_type', 'service_type']
+    list_editable = ['detail_type', 'service_type', 'description', 'normal_day', 'new_day', 'note', 'waste_area_ids']
+    # search_fields = ['reason', 'note']
+    ordering = ['normal_day', 'new_day']
+
+    # formfield_overrides = {
+    #         models.TextField: {'widget': Textarea(
+    #                            attrs={'rows': 1,
+    #                                   'cols': 200,
+    #                                   'style': 'height: 1em;'})},
+    #     }
+
+
+# admin.site.register(ScheduleDetail, ScheduleDetailAdmin)
 admin.site.register(ScheduleChange, ScheduleChangeAdmin)
