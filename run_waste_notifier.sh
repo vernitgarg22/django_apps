60aca0280e0a9278c6f2b75df204714fa68029be
karlkaebnick <karl.kaebick@gmail.com>
2017-04-11 16:41:53 -0400

    Waste Notifier:  simplify automation script

--- run_waste_notifier.sh
+++ run_waste_notifier.sh
@@ -1,4 +1,4 @@
-OUTPUT="c:/cygwin64/home/kaebnickk/output.txt"
+OUTPUT="waste_notifier_output.txt"
 
 echo $(date) >> ${OUTPUT}
 curl --silent https://apis.detroitmi.gov/waste_notifier/send/ >> ${OUTPUT}
