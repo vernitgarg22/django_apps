3caf3cd54e4a4fa164604c165cd046fa48a6997f
karlkaebnick <karl.kaebick@gmail.com>
2017-04-11 17:01:35 -0400

    Waste Notifier: fix path to output file

--- run_waste_notifier.sh
+++ run_waste_notifier.sh
@@ -1,4 +1,4 @@
-OUTPUT="waste_notifier_output.txt"
+OUTPUT="${DJANGO_HOME}/waste_notifier_output.txt"
 
 echo $(date) >> ${OUTPUT}
 curl --silent https://apis.detroitmi.gov/waste_notifier/send/ >> ${OUTPUT}
