45e61df7979042c2d7b7a3420f9ef4681a7b5511
karlkaebnick <karl.kaebick@gmail.com>
2017-04-13 16:32:17 -0400

    Waste Notifier:  /send is now 'post' only

--- run_waste_notifier.sh
+++ run_waste_notifier.sh
@@ -1,7 +1,7 @@
 OUTPUT="${DJANGO_HOME}/waste_notifier_output.txt"
 
 echo $(date) >> ${OUTPUT}
-curl --silent https://apis.detroitmi.gov/waste_notifier/send/ >> ${OUTPUT}
+curl -X POST https://apis.detroitmi.gov/waste_notifier/send/ >> ${OUTPUT}
 err_code=$?
 echo "" >> ${OUTPUT}
 echo "error code: ${err_code}" >> ${OUTPUT}
