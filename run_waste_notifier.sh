9e6d47808ddd9949d9d8b5c0b9133bf34bc1e7a7
karlkaebnick <karl.kaebick@gmail.com>
2017-04-13 16:04:56 -0400

    Waste Notifier:  more info output by waste notifier shell script

--- run_waste_notifier.sh
+++ run_waste_notifier.sh
@@ -2,5 +2,7 @@ OUTPUT="${DJANGO_HOME}/waste_notifier_output.txt"
 
 echo $(date) >> ${OUTPUT}
 curl --silent https://apis.detroitmi.gov/waste_notifier/send/ >> ${OUTPUT}
+err_code=$?
 echo "" >> ${OUTPUT}
-
+echo "error code: ${err_code}" >> ${OUTPUT}
+echo "" >> ${OUTPUT}
