6af0a6342ed5604b8509c1cde9a7adbcbbbdadef
karlkaebnick <karl.kaebick@gmail.com>
2017-04-23 12:49:58 -0400

    Waste Notifier:  pass in desired current day

--- run_waste_notifier.sh
+++ run_waste_notifier.sh
@@ -1,7 +1,9 @@
 OUTPUT="${DJANGO_HOME}/waste_notifier_output.txt"
 
 echo $(date) >> ${OUTPUT}
-curl -X POST https://apis.detroitmi.gov/waste_notifier/send/ >> ${OUTPUT}
+date_str=$(date "+%Y%m%d")
+url="https://apis.detroitmi.gov/waste_notifier/send/?today=${date_str}"
+curl -X POST ${url} >> ${OUTPUT}
 err_code=$?
 echo "" >> ${OUTPUT}
 echo "error code: ${err_code}" >> ${OUTPUT}
