37c230388abaa6629bcd7c38b4ebbac1b7635a04
karlkaebnick <karl.kaebick@gmail.com>
2017-04-27 18:04:21 -0400

    Waste Schedule:  restore a url that got removed accidentally

--- waste_schedule/urls.py
+++ waste_schedule/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'waste_schedule'
 urlpatterns = [
     url(r'^details/(?P<waste_area_ids>[0-9,]*)/$', views.get_schedule_details),
-    url(r'^details/(?P<waste_area_ids>[0-9,]*)/$', views.get_schedule_details),
     url(r'^details/(?P<waste_area_ids>[0-9,]*)/year/(?P<year>[0-9]{4})/$', views.get_schedule_details),
+    url(r'^details/(?P<waste_area_ids>[0-9,]*)/year/(?P<year>[0-9]{4})/month/(?P<month>[0-9]+)/$', views.get_schedule_details),
 ]
