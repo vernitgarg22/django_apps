41f304be207bd695c1677f2cc7648eac3080992c
karlkaebnick <karl.kaebick@gmail.com>
2017-05-08 16:39:32 -0400

    Django Apps: clean up run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -12,12 +12,14 @@ if __name__ == "__main__":
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
+    # test_labels = ["tests"]
     # failures = test_runner.run_tests(["tests"])
-    # failures = test_runner.run_tests(["tests.cod_utils_tests"])
-    # failures = test_runner.run_tests(["tests.test_waste_schedule"])
-    # failures = test_runner.run_tests(["tests.test_waste_schedule.WasteScheduleTests.test_get_schedule_details_citywide_reschedule"])
-    # failures = test_runner.run_tests(["tests.test_waste_notifier"])
-    # failures = test_runner.run_tests(["tests.test_waste_wizard"])
-    # failures = test_runner.run_tests(["tests.test_waste_notifier.WasteNotifierTests.test_confirm_invalid_phone_number"])
-    failures = test_runner.run_tests(["tests.cod_utils_tests", "tests.test_waste_schedule", "tests.test_waste_notifier", "tests.test_waste_wizard", "tests.test_weather_info"])
+    # test_labels = ["tests.cod_utils_tests"]
+    # test_labels = ["tests.test_waste_schedule"]
+    # test_labels = ["tests.test_waste_schedule.WasteScheduleTests.test_get_schedule_details_citywide_reschedule"]
+    # test_labels = ["tests.test_waste_notifier"]
+    # test_labels = ["tests.test_waste_wizard"]
+    # test_labels = ["tests.test_waste_notifier.WasteNotifierTests.test_confirm_invalid_phone_number"]
+    test_labels = ["tests.cod_utils_tests", "tests.test_waste_schedule", "tests.test_waste_notifier", "tests.test_waste_wizard", "tests.test_weather_info"]
+    failures = test_runner.run_tests(test_labels)
     sys.exit(bool(failures))
