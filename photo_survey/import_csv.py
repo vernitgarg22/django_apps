cb9611a1234736d1342814dbdb53e8bacc7330b2
karlkaebnick <karl.kaebick@gmail.com>
2017-06-26 13:57:34 -0400

    Photo Survey:  add new img metadata fields

--- photo_survey/import_csv.py
+++ photo_survey/import_csv.py
@@ -2,13 +2,25 @@ import csv
 
 from photo_survey.models import Image, ImageMetadata
 from datetime import datetime
+from decimal import Decimal
+
+
+# python manage.py shell
+# from photo_survey import import_csv; import_csv.run_import('survey.csv')
 
 
 def clean_string(buffer):
+    """
+    Trim quotes from start and end of string
+    """
     if len(buffer) >= 2:
         return buffer[1:-1]
     else:
         return ''
+
+
+def clean_decimal(buffer):
+    return Decimal(clean_string(buffer))
 
 
 def run_import(csv_file_path):
@@ -25,17 +37,37 @@ def run_import(csv_file_path):
 
                 # print(', '.join(row))
 
-                filename = clean_string(row[0])
-                parcel_id = clean_string(row[1])
-                created_at = datetime.strptime(clean_string(row[2]), "%Y:%m:%d %H:%M:%S")
-                path = clean_string(row[3])
-                subdir = path.split('\\')[6]
+                # 0           1           2           3           4           5           6           7           8           9           10          11      12
+                # filepath    filename    longitude   latitude    altitude    gps_date    img_date    parcelno    house_numb  street_nam  street_typ  zipcode common_name
+
+                filename = clean_string(row[1])
+                parcel_id = clean_string(row[7])
+                created_at = datetime.strptime(clean_string(row[5]), "%Y:%m:%d %H:%M:%S")
+                path = clean_string(row[0])
+                subdir = path.split('/')[3]
+                latitude = clean_decimal(row[3])
+                longitude = clean_decimal(row[2])
+                altitude = clean_decimal(row[4])
+
+                house_number = clean_string(row[8])
+                street_name = clean_string(row[9])
+                street_type = clean_string(row[10])
+                zipcode = clean_string(row[11])
+                common_name = clean_string(row[12])
 
                 file_path = subdir + '/' + filename
+
+                # print(parcel_id + ' - ' + file_path)
+
                 if not files.get(file_path):
                     img = Image(file_path=file_path)
                     img.save()
-                    img_meta = ImageMetadata(image=img, parcel_id=parcel_id, created_at=created_at)
+
+                    # TODO: get lat/long/altitude working
+                    # latitude=latitude, longitude=longitude, altitude=altitude,
+
+                    img_meta = ImageMetadata(image=img, parcel_id=parcel_id, created_at=created_at, 
+                                    house_number=house_number, street_name=street_name, street_type=street_type, zipcode=zipcode, common_name=common_name)
                     img_meta.save()
 
                     files[file_path] = True
