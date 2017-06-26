eeb1561f06a89bf5e478d6f2498e8a43ca32ed5e
karlkaebnick <karl.kaebick@gmail.com>
2017-06-26 18:47:40 -0400

    Photo Survey: add trigger action fields to admin

--- photo_survey/admin.py
+++ photo_survey/admin.py
@@ -5,13 +5,13 @@ from .models import SurveyTemplate
 
 class SurveyTemplateAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields': ['survey_template_id', 'question_id', 'question_number', 'question_text', 'valid_answers', 'required_by', 'required_by_answer']}),
+        (None, {'fields': ['survey_template_id', 'question_id', 'question_number', 'question_text', 'valid_answers', 'required_by', 'required_by_answer', 'answer_trigger', 'answer_trigger_action']}),
         # ('Destination', {'fields': ['destination'], 'classes': ['collapse']}),
     ]
     # inlines = [ChoiceInline]
-    list_display = ('survey_template_id', 'question_id', 'question_number', 'question_text', 'valid_answers', 'required_by', 'required_by_answer')
+    list_display = ('survey_template_id', 'question_id', 'question_number', 'question_text', 'valid_answers', 'required_by', 'required_by_answer', 'answer_trigger', 'answer_trigger_action')
     list_filter = ['survey_template_id']
-    list_editable = ['survey_template_id', 'question_id', 'question_number', 'question_text', 'valid_answers', 'required_by', 'required_by_answer']
+    list_editable = ['survey_template_id', 'question_id', 'question_number', 'question_text', 'valid_answers', 'required_by', 'required_by_answer', 'answer_trigger', 'answer_trigger_action']
     search_fields = ['survey_template_id', 'question_id', 'question_text']
     ordering = ['survey_template_id', 'question_number']
 
