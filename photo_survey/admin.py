a4a3236f50e77057deed85d6127a5248114c8902
karlkaebnick <karl.kaebick@gmail.com>
2017-06-27 11:41:07 -0400

    Photo Survey:  oops survey_template_id should not be editable

--- photo_survey/admin.py
+++ photo_survey/admin.py
@@ -11,7 +11,7 @@ class SurveyTemplateAdmin(admin.ModelAdmin):
     # inlines = [ChoiceInline]
     list_display = ('survey_template_id', 'question_id', 'question_number', 'question_text', 'valid_answers', 'required_by', 'required_by_answer', 'answer_trigger', 'answer_trigger_action')
     list_filter = ['survey_template_id']
-    list_editable = ['survey_template_id', 'question_id', 'question_number', 'question_text', 'valid_answers', 'required_by', 'required_by_answer', 'answer_trigger', 'answer_trigger_action']
+    list_editable = ['question_id', 'question_number', 'question_text', 'valid_answers', 'required_by', 'required_by_answer', 'answer_trigger', 'answer_trigger_action']
     search_fields = ['survey_template_id', 'question_id', 'question_text']
     ordering = ['survey_template_id', 'question_number']
 
