972a4e36b6a53cd4212ddb84908c825881078570
karlkaebnick <karl.kaebick@gmail.com>
2017-06-27 10:11:58 -0400

    Photo Survey:  add TODOs to keep track of stuff

--- django_apps/settings.py
+++ django_apps/settings.py
@@ -19,6 +19,15 @@ from django.core.exceptions import ImproperlyConfigured
 
 # TODO move stuff that is shared between django_apps.settings and tests.test_settings into a shared location
 
+# TODO upgrade to django 1.11
+# https://docs.djangoproject.com/en/1.11/howto/upgrade-version/
+# http://django.readthedocs.io/en/latest/howto/upgrade-version.html
+
+# TODO start using django-pyodbc-azure to connect to sql-server
+# https://github.com/michiya/django-pyodbc-azure
+# https://docs.microsoft.com/en-us/sql/relational-databases/native-client/sql-server-native-client
+# https://github.com/mkleehammer/pyodbc/wiki/Connecting-to-SQL-Server-from-Windows
+
 
 DJANGO_HOME = os.environ['DJANGO_HOME']
 
