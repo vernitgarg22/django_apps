ea97d22eba9d852a44c4fba1c45bc75217e8719e
karlkaebnick <karl.kaebick@gmail.com>
2017-07-12 16:20:04 -0400

    Photo Survey:  add ownership data as an option on survey data export

--- photo_survey/management/commands/export_survey_answers.py
+++ photo_survey/management/commands/export_survey_answers.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from django.core.management.base import BaseCommand, CommandError
 
 from photo_survey.models import Survey, SurveyAnswer, SurveyQuestion
+from assessments.models import ParcelMaster
 
 
 class Command(BaseCommand):
@@ -12,15 +13,23 @@ class Command(BaseCommand):
         python manage.py export_survey_answers survey_template_id """
 
     def add_arguments(self, parser):
-        parser.add_argument('survey_template_id', type=str)
+        parser.add_argument('survey_template_id', type=str, help='Identifies the survey type')
+        parser.add_argument('--pretty_print', default='y', help='Pretty print values')
+        parser.add_argument('--add_data', default='ownership', help='Comma-delimited set of types of data to add')
 
     def init_metadata(self, options):
         """
         Initializes internal data to do the export.
         """
 
-        self.using_db='photo_survey'
+        # First parse out command-line options
+        self.pretty_print = options['pretty_print'] == 'y'
+        tmp = options.get('add_data', '')
+        self.data_types = { data_type: True for data_type in tmp.split(',') }
         self.survey_template_id = options['survey_template_id']
+
+        # Now finish initializing everything
+        self.using_db='photo_survey'
         self.questions = SurveyQuestion.objects.using(self.using_db).filter(survey_template_id=self.survey_template_id).order_by('question_number')
         now = datetime.now()
         self.out_file = now.strftime("%Y%m%d_%H%M%S.csv")
@@ -34,6 +43,53 @@ class Command(BaseCommand):
         self.surveys = Survey.objects.using(self.using_db).filter(survey_template_id=self.survey_template_id).order_by('parcel_id')
         self.answers = SurveyAnswer.objects.using(self.using_db).all()
 
+    def get_fieldnames(self):
+        """
+        Returns correct header row column names.
+        """
+
+        field_names = [ 'parcel', 'surveyor', 'common_name', 'note', 'status', 'created_at' ] + [ question.question_id for question in self.questions ]
+        if self.data_types.get('ownership', False):
+            field_names.extend( [ 'owner name', 'owner address', 'owner city', 'owner state', 'owner zip' ] )
+
+        return field_names
+
+    def get_answerdata(self, survey):
+        """
+        Returns answer data for a given survey.
+        """
+
+        curr_answers = [ answer for answer in self.answers if answer.survey_id == survey.id ]
+        if not curr_answers:
+            return None
+
+        # Get basic information about the survey itself
+        answer_data = { 'parcel': survey.parcel_id, 'surveyor': survey.user.username, 'common_name': survey.common_name, 'note': survey.note, 'status': survey.status, 'created_at': survey.created_at }
+
+        # Get the answers
+        answer_data_tmp = { answer.question_id: answer.answer for answer in curr_answers }
+
+        # TODO pretty print the answers?
+        # if self.pretty_print:
+
+        answer_data.update(answer_data_tmp)
+
+        # Get the ownership data?
+        if self.data_types.get('ownership', False):
+            parcels = ParcelMaster.objects.filter(pnum=survey.parcel_id)
+            if parcels:
+                parcel = parcels[0]
+                owner_name = parcel.ownername1
+                if parcel.ownername2:
+                    owner_name = owner_name + ' - ' + parcel.ownername2
+                answer_data['owner name'] = owner_name
+                answer_data['owner address'] = parcel.ownerstreetaddr
+                answer_data['owner city'] = parcel.ownercity
+                answer_data['owner state'] = parcel.ownerstate
+                answer_data['owner zip'] = parcel.ownerzip
+
+        return answer_data
+
     def export_data(self):
         """
         Export the actual data
@@ -41,19 +97,19 @@ class Command(BaseCommand):
 
         with open(self.out_file, 'w', newline='') as csvfile:
 
-            fieldnames = [ 'parcel', 'surveyor', 'common_name', 'note', 'status', 'created_at' ] + [ question.question_id for question in self.questions ]
+            fieldnames = self.get_fieldnames()
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
 
             for survey in self.surveys:
-                id = survey.id
-                curr_answers = [ answer for answer in self.answers if answer.survey_id == id ]
-                if curr_answers:
-                    answer_data = { 'parcel': survey.parcel_id, 'surveyor': survey.user.username, 'common_name': survey.common_name, 'note': survey.note, 'status': survey.status, 'created_at': survey.created_at }
-                    answer_data.update( { answer.question_id: answer.answer for answer in curr_answers } )
+                answer_data = self.get_answerdata(survey)
+                if answer_data:
                     writer.writerow(answer_data)
+                    self.num_exported = self.num_exported + 1
 
-                    self.num_exported = self.num_exported + len(curr_answers)
+                    if self.num_exported % 20 == 0:
+                        self.stdout.write("{} rows exported ...".format(self.num_exported))
+                        self.stdout.flush()
 
     def handle(self, *args, **options):
 
@@ -63,4 +119,4 @@ class Command(BaseCommand):
 
         self.export_data()
 
-        return "Exported {} rows to {}".format(self.num_exported, self.out_file)
+        return "Exported answers for {} surveys to {}".format(self.num_exported, self.out_file)
