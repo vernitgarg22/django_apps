b99eef64f1b1f59272a60501d8ccc43e4461f562
karlkaebnick <karl.kaebick@gmail.com>
2017-10-03 10:19:39 -0400

    Data Cache:  add admin classes

--- data_cache/admin.py
+++ data_cache/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DataCredentials, DataSource, DataValue
+
+
+class DataCredentialsAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None, {'fields': ['username', 'password', 'referer', 'url']}),
+    ]
+    list_display = ('username', 'password', 'referer', 'url')
+    list_filter = ['url']
+    list_editable = ['password', 'referer', 'url']
+    search_fields = ['url']
+    ordering = ['url']
+
+admin.site.register(DataCredentials, DataCredentialsAdmin)
+
+class DataSourceAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None, {'fields': ['name', 'url', 'credentials']}),
+    ]
+    list_display = ('name', 'url', 'credentials')
+    list_filter = ['url']
+    list_editable = ['url', 'credentials']
+    search_fields = ['url']
+    ordering = ['url']
+
+admin.site.register(DataSource, DataSourceAdmin)
