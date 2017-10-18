624945f5c44ca3d4e4b6e14cf185392fb4ca3c03
karlkaebnick <karl.kaebick@gmail.com>
2017-10-18 18:47:59 -0400

    Data Cache:  almost done with changes to add ability to query data values by param

--- data_cache/models.py
+++ data_cache/models.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+from django.conf import settings
 from django.db import models
 from rest_framework import status
 from rest_framework.response import Response
@@ -48,19 +49,70 @@ class DataSource(models.Model):
     name = models.CharField('Name', max_length=64, unique=True, db_index=True)
     url = models.CharField('Data Source URL', max_length=1024, null=True, blank=True)
     credentials = models.ForeignKey(DataCredential, null=True, blank=True)
+    data_parse_path = models.CharField('Data to extract', max_length=128, null=True, blank=True)
+    data_id_parse_path = models.CharField('Data ID to extract', max_length=128, null=True, blank=True)
 
-    def get(self, params=None):
+    def refresh(self):
+        """
+        Update all data values for this data source.
+        """
+
+        # Is the data for this data source set once, manually?
+        if self.is_static():
+            if not self.datavalue_set.exists():
+                DataValue(data_source=self).save()
+            return
+
+        # Get the data
+        url = self.get_url()
+        r = requests.get(url)
+
+        # Test success and attempt to parse
+        if not status.is_success(r.status_code):    # pragma: no cover (exception-handling should prevent us from ever getting here)
+            raise Exception("Data source {} not available".format(self.data_source.url))
+
+        try:
+
+            data = r.json()
+            if self.is_multiple():
+
+                for idx, sub_data in enumerate(data[self.data_parse_path]):
+
+                    sub_data_tmp = sub_data
+                    for data_id_key in self.data_id_parse_path.split('/'):
+                        sub_data_tmp = sub_data_tmp[data_id_key]
+
+                    data_value_id = sub_data_tmp
+                    data_value, success = self.datavalue_set.get_or_create(param=data_value_id)
+
+                    data_value.data = json.dumps(sub_data)
+                    data_value.save(force_update=True)
+
+                    if settings.RUNNING_UNITTESTS and idx > 100:
+                        break
+
+            else:
+                data_value, success = self.datavalue_set.get_or_create(id=1)
+                data_value.data = json.dumps(data)
+                data_value.save(force_update=True)
+
+        except json.decoder.JSONDecodeError:
+            raise Exception("Invalid JSON received")
+
+    def get(self, param=None):
         """
         Refreshes the data (if needed) and returns the datavalue object.
         """
-        if self.datavalue_set.exists():
-            if params:
-                data_values = self.datavalue_set.filter(tag=tag)
-            data_value = self.datavalue_set.first()
-        else:
 
-            data_value = DataValue(data_source=self, tag=tag)
-            data_value.update()
+        # Retrieve the data, if necessary
+        if not self.datavalue_set.exists():
+            self.refresh()
+
+        # Now return the requested data_value
+        if param:
+            data_value = self.datavalue_set.filter(param=param).first()
+        else:
+            data_value = self.datavalue_set.first()
 
         return data_value
 
@@ -71,6 +123,36 @@ class DataSource(models.Model):
         """
 
         return not self.url
+
+    def is_multiple(self):
+        """
+        Returns True if there can be more than 1 data value for this data source.
+        """
+
+        return self.data_parse_path and self.data_id_parse_path
+
+    def get_url(self):
+        """
+        Gets url, including auth token (if needed).
+        """
+
+        url = self.url
+
+        token = None
+        if self.credentials:
+            success, token = self.credentials.create_auth_token()
+            if False == success:
+                raise Exception("Authentication failed")
+
+        # Add auth token to url?
+        if token:
+            if url.find("?") < 0:
+                url = url + "?"
+            else:
+                url = url + "&"
+            url = url + "token={}".format(token)
+
+        return url
 
     def __str__(self):    # pragma: no cover (mostly for debugging)
         return self.name
@@ -86,9 +168,7 @@ class DataValue(models.Model):
     data_source = models.ForeignKey(DataSource)
     data = models.TextField()
     updated = models.DateTimeField('Last time data was cached')
-
-    # TODO finish this?
-    tag = models.CharField(max_length=128, blank=True, null=True)
+    param = models.CharField(max_length=128, blank=True, null=True, db_index=True)
 
     def save(self, *args, **kwargs):
         """
@@ -99,56 +179,6 @@ class DataValue(models.Model):
 
         # Call the "real" save() method in base class
         super().save(*args, **kwargs)
-
-    def get_url(self):
-        """
-        Gets url, including auth token (if needed).
-        """
-
-        url = self.data_source.url
-
-        token = None
-        if self.data_source.credentials:
-            success, token = self.data_source.credentials.create_auth_token()
-            if False == success:
-                raise Exception("Authentication failed")
-
-        # Add auth token to url?
-        if token:
-            if url.find("?") < 0:
-                url = url + "?"
-            else:
-                url = url + "&"
-            url = url + "token={}".format(token)
-
-        return url
-
-    def update(self):
-        """
-        Update the data for this data value instance.
-        """
-
-        if self.data_source.is_static():
-            return
-
-        # Get the data
-        url = self.get_url()
-
-        # add in url param?
-        if self.tag:
-            url = url.format(tag)
-
-        r = requests.get(url)
-
-        # Test success and attempt to parse
-        if not status.is_success(r.status_code):    # pragma: no cover (exception-handling should prevent us from ever getting here)
-            raise Exception("Data source {} not available".format(self.data_source.url))
-
-        try:
-            self.data = json.dumps(r.json())
-            self.save()
-        except json.decoder.JSONDecodeError:
-            raise Exception("Invalid JSON received")
 
     def __str__(self):    # pragma: no cover (mostly for debugging)
         return self.data_source.name
