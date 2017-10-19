ffdb419feab750676a394b0a006f1c09412f5ed0
karlkaebnick <karl.kaebick@gmail.com>
2017-10-19 11:17:02 -0400

    Data Cache:  add param to data value admin

--- data_cache/admin.py
+++ data_cache/admin.py
@@ -19,11 +19,11 @@ admin.site.register(DataCredential, DataCredentialAdmin)
 class DataSourceAdmin(admin.ModelAdmin):
 
     fieldsets = [
-        (None, {'fields': ['name', 'url', 'credentials']}),
+        (None, {'fields': ['name', 'url', 'credentials', 'data_parse_path', 'data_id_parse_path']}),
     ]
-    list_display = ('name', 'url', 'credentials')
+    list_display = ('name', 'url', 'credentials', 'data_parse_path', 'data_id_parse_path')
     list_filter = ['url']
-    list_editable = ['url', 'credentials']
+    list_editable = ['url', 'credentials', 'data_parse_path', 'data_id_parse_path']
     search_fields = ['url']
     ordering = ['url']
 
@@ -32,9 +32,9 @@ admin.site.register(DataSource, DataSourceAdmin)
 class DataValueAdmin(admin.ModelAdmin):
 
     fieldsets = [
-        (None, {'fields': ['data_source', 'data', 'updated']}),
+        (None, {'fields': ['data_source', 'data', 'updated', 'param']}),
     ]
-    list_display = ('data_source', 'data', 'updated')
+    list_display = ('data_source', 'data', 'updated', 'param')
     list_filter = ['data_source']
     list_editable = ['data', 'updated']
     search_fields = ['data_source']
