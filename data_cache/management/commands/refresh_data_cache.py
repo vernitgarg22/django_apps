8f551ea011e79578876ca30cdc793f878cbd3c32
karlkaebnick <karl.kaebick@gmail.com>
2017-11-14 16:28:06 -0500

    Data Cache:  oops remove unused import

--- data_cache/management/commands/refresh_data_cache.py
+++ data_cache/management/commands/refresh_data_cache.py
@@ -1,5 +1,4 @@
 from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures import as_completed
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
