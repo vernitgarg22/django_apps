c403840c4c0769f702b5ef318639da478aeda86f
Karl Kaebnick <karl.kaebnick@gmail.com>
2017-12-05 18:07:04 -0500

    Waste Notifier:  improve subscriber admin page

--- waste_notifier/admin.py
+++ waste_notifier/admin.py
@@ -10,9 +10,10 @@ admin.site.disable_action('delete_selected')
 class SubscriberAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['phone_number', 'waste_area_ids', 'status', 'service_type', 'comment', 'last_status_update']}),
+        ('Advanced', { 'classes': ('collapse',), 'fields': ('address', 'latitude', 'longitude') }),
     ]
     list_display = ('phone_number', 'status', 'service_type', 'comment', 'last_status_update')
-    list_filter = ['status', 'comment']
+    list_filter = ['status']
     list_editable = ['status', 'service_type', 'comment']
     search_fields = ['phone_number', 'waste_area_ids', 'status', 'service_type', 'comment']
     ordering = ['-last_status_update']
