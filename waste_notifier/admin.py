9b6707da86f891a5122061c47d81c1adb57802d8
Karl Kaebnick <karl.kaebnick@gmail.com>
2018-02-01 14:36:35 -0500

    Waste Notifier:  tweak admin (address & service type)

--- waste_notifier/admin.py
+++ waste_notifier/admin.py
@@ -9,13 +9,13 @@ admin.site.disable_action('delete_selected')
 
 class SubscriberAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields': ['phone_number', 'waste_area_ids', 'status', 'service_type', 'comment', 'last_status_update']}),
-        ('Advanced', { 'classes': ('collapse',), 'fields': ('address', 'latitude', 'longitude') }),
+        (None, {'fields': ['phone_number', 'waste_area_ids', 'status', 'address', 'comment', 'last_status_update']}),
+        ('Advanced', { 'classes': ('collapse',), 'fields': ('service_type', 'latitude', 'longitude') }),
     ]
-    list_display = ('phone_number', 'status', 'service_type', 'comment', 'last_status_update')
+    list_display = ('phone_number', 'status', 'address', 'comment', 'last_status_update')
     list_filter = ['status']
-    list_editable = ['status', 'service_type', 'comment']
-    search_fields = ['phone_number', 'waste_area_ids', 'status', 'service_type', 'comment']
+    list_editable = ['status', 'address', 'comment']
+    search_fields = ['phone_number', 'waste_area_ids', 'status', 'address', 'comment', 'service_type']
     ordering = ['-last_status_update']
 
 admin.site.register(Subscriber, SubscriberAdmin)
