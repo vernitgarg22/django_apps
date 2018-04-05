14dca27c1c9e97498ecf3aedf609ea7523123266
Karl Kaebnick <karl.kaebnick@gmail.com>
2018-04-05 11:27:30 -0400

    Django Apps:  clean up run coverage

--- run_coverage.py
+++ run_coverage.py
@@ -2,13 +2,11 @@
 import subprocess
 
 
-class run_cmd():
+def run_cmd(cmd, errmsg):
 
-    def __init__(self, cmd, errmsg):
-
-        return_value = subprocess.run(cmd, shell=True)
-        if return_value.returncode != 0:
-            raise Exception(errmsg)
+    return_value = subprocess.run(cmd, shell=True)
+    if return_value.returncode != 0:
+        raise Exception(errmsg)
 
 run_cmd("coverage run run_tests.py", "One or more tests failed")
 run_cmd("coverage html", "Error generating coverage html")
