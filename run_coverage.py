2453c71918ab2074389cb88a598d1022bbca3342
Karl Kaebnick <karl.kaebnick@gmail.com>
2018-04-05 11:22:29 -0400

    Django Apps:  clean up run coverage

--- run_coverage.py
+++ run_coverage.py
@@ -1,15 +1,15 @@
-coverage run run_tests.py
-if [ $? != 0 ]
-then
-    echo "One or more tests failed"
-    exit 1
-fi
-coverage html
-which start > /dev/null
-retval=$?
-if [ ${retval} -eq 0 ]
-then
-	start htmlcov/index.html
-else
-	open htmlcov/index.html
-fi
+#!/usr/bin/env python
+import subprocess
+
+
+class run_cmd():
+
+    def __init__(self, cmd, errmsg):
+
+        return_value = subprocess.run(cmd, shell=True)
+        if return_value.returncode != 0:
+            raise Exception(errmsg)
+
+run_cmd("coverage run run_tests.py", "One or more tests failed")
+run_cmd("coverage html", "Error generating coverage html")
+run_cmd("start htmlcov/index.html", "Error opening coverage report")
