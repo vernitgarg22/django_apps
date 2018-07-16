d4b561b3c1925d5991a763e951218d45cdb92bf4
karlkaebnick <karl.kaebnick@gmail.com>
2018-07-16 12:10:52 -0400

    Rework translations exporter a bunch

--- export_translations.py
+++ export_translations.py
@@ -18,13 +18,23 @@ import pdb
 
 
 server = "http://detroitmi.theneighborhoods.org"
+
+# "/Government/Boards",
+# "/Government/Commissions",
+# "/How-Do-I/Neighborhood-Police-Officer-NPO-program/7TH-PRECINCT-NPO",
+# "/How-Do-I/Neighborhood-Police-Officer-NPO-program/8TH-PRECINCT-NPO",
+# "/How-Do-I/Neighborhood-Police-Officer-NPO-program/9TH-PRECINCT-NPO",
+# "/How-Do-I/Neighborhood-Police-Officer-NPO-program/GAMING-NPO",
+# "/Calendar-and-Events",
+# "/Calendar-Events",
+# "/Government/City-Council/George-Cushingberry",
+
+
 urls = [
     "/2016-Crime-Statistics",
     "/Boards/BoardOfPoliceCommissioners",
     "/BSEED",
     "/businessincometax",
-    "/Calendar-and-Events",
-    "/Calendar-Events",
     "/CampauBanglatown",
     "/Correspondence",
     "/DACC",
@@ -47,8 +57,6 @@ urls = [
     "/elections",
     "/employment",
     "/fintreasury",
-    "/Government",
-    "/Government/Boards",
     "/Government/Boards/Board-of-Electrical-Examiners-Forms",
     "/Government/City-Clerk",
     "/Government/City-Council",
@@ -60,9 +68,7 @@ urls = [
     "/Government/City-Council/Janee-L-Ayers",
     "/Government/City-Council/Mary-Sheffield",
     "/Government/City-Council/Raquel-Castaneda-Lopez",
-    "/Government/City-Council/Roy-McCalister",
     "/Government/City-Council/Scott-Benson",
-    "/Government/Commissions",
     "/Government/Departments",
     "/Government/Departments-and-Agencies/Detroit-Department-of-Transportation/DDOT-News-and-Alerts",
     "/Government/Departments-and-Agencies/Detroit-Health-Department/Detroit-ID",
@@ -109,6 +115,12 @@ urls = [
     "/How-Do-I/File/Income-Tax-Forms",
     "/How-Do-I/File/Income-Tax-Information",
     "/How-Do-I/Find",
+    "/How-Do-I/Find-Community-Services",
+    "/How-Do-I/Find-Community-Services/Arson-Awareness",
+    "/How-Do-I/Find-Community-Services/Keep-the-Water-On",
+    "/How-Do-I/Find-Detroit-Archives",
+    "/How-Do-I/Find-Detroit-Archives/City-Clerks-Archives-and-Records-Information",
+    "/How-Do-I/Find-Transportation/Buy-A-Pass",
     "/How-Do-I/Find/Adams-Butzel-Complex",
     "/How-Do-I/Find/Birth-and-Death-Certificates",
     "/How-Do-I/Find/City-Employee-Information",
@@ -129,12 +141,6 @@ urls = [
     "/How-Do-I/Find/Refuse-Collection",
     "/How-Do-I/Find/Schedule-An-Appointment",
     "/How-Do-I/Find/Water-Sewer-and-Drainage-Rates-101",
-    "/How-Do-I/Find-Community-Services",
-    "/How-Do-I/Find-Community-Services/Arson-Awareness",
-    "/How-Do-I/Find-Community-Services/Keep-the-Water-On",
-    "/How-Do-I/Find-Detroit-Archives",
-    "/How-Do-I/Find-Detroit-Archives/City-Clerks-Archives-and-Records-Information",
-    "/How-Do-I/Find-Transportation/Buy-A-Pass",
     "/How-Do-I/Grants",
     "/How-Do-I/Index-A-to-Z/A",
     "/How-Do-I/Index-A-to-Z/P",
@@ -143,16 +149,12 @@ urls = [
     "/How-Do-I/Locate-Transportation/Bus-Schedules-Information",
     "/How-Do-I/Locate-Transportation/Metro-Lift-Requirements",
     "/How-Do-I/Locate-Transportation/Public-Notices",
-    "/How-Do-I/Locate-Transportation/Transporation-Fares",
+    "/How-Do-I/Locate-Transportation/Transportation-Fares",
     "/How-Do-I/Medical-Marihuana-Information",
+    "/ImproveDetroit",
     "/How-Do-I/MobileApps",
-    "/How-Do-I/Mobile-Apps/ImproveDetroit",
     "/How-Do-I/My-District",
     "/How-Do-I/Neighborhood-Police-Officer-NPO-program",
-    "/How-Do-I/Neighborhood-Police-Officer-NPO-program/7TH-PRECINCT-NPO",
-    "/How-Do-I/Neighborhood-Police-Officer-NPO-program/8TH-PRECINCT-NPO",
-    "/How-Do-I/Neighborhood-Police-Officer-NPO-program/9TH-PRECINCT-NPO",
-    "/How-Do-I/Neighborhood-Police-Officer-NPO-program/GAMING-NPO",
     "/How-Do-I/Obtain-Grant-Information/Home-Repair-Program-Information",
     "/How-Do-I/Obtain-Grant-Information/Renaissance-Zones",
     "/How-Do-I/Pay",
@@ -200,172 +202,244 @@ urls = [
     "/youthprograms",
 ]
 
-output_errs = False
-export_cnt = {}
-error_cnt = {}
 
+class ContentExporter():
 
-def report_err(msg, desc):
-    if output_errs:
-        print("ERROR:  " + msg)
-    cnt = error_cnt.get(desc, 1)
-    error_cnt[desc] = cnt + 1
+    output_errs = False
+    error_cnt = {}
+    urls_exported = {}
 
-def report_err_cnt():
-    if output_errs:
-        print('\n**************************************************************************************\n')
-        print('errors:  ' + str(error_cnt))
-        print('num successful exports: ' + str(len(export_cnt)))
+    @staticmethod
+    def report_err(msg, desc):
+        if ContentExporter.output_errs:
+            print("ERROR:  " + msg)
+        cnt = ContentExporter.error_cnt.get(desc, 1)
+        ContentExporter.error_cnt[desc] = cnt + 1
 
-def in_review(content):
+    @staticmethod
+    def report_err_cnt():
+        if ContentExporter.output_errs:
+            print('\n**************************************************************************************\n')
+            print('errors:  ' + str(ContentExporter.error_cnt))
+            print('num successful exports: ' + str(len(ContentExporter.urls_exported)))
 
-    for key in [ 'field_need_review', 'field_need_reviewed' ]:
-        if content.get(key) and content[key][0]['value']:
-            return True
+    @staticmethod
+    def in_review(content):
 
-    tmp_content = str(content)
-    for word in [ 'TODO', 'REVIEW' ]:
-        if word in tmp_content:
-            return True
+        for key in [ 'field_need_review', 'field_need_reviewed' ]:
+            if content.get(key) and content[key][0]['value']:
+                return True
 
-    return False
+        tmp_content = str(content)
+        for word in [ 'TODO', 'REVIEW' ]:
+            if word in tmp_content:
+                return True
 
-def needs_translation(content):
+        return False
 
-    tmp = content['changed'][0]['value']
-    date_changed = datetime.strptime(tmp[0 : 10], '%Y-%m-%d').date()
+    @staticmethod
+    def needs_translation(content):
 
-    for key in [ "field_dept_translation_date", "field_gov_translation_date" ]:
-
-        if content.get(key):
-
-            tmp = content[key][0]['value']
-            translation_date = datetime.strptime(tmp, '%Y-%m-%d').date()
-            return translation_date < date_changed
-
-    return True
-
-def strip_html(content):
-
-    begin = content.find("<")
-    if begin == -1:
-        return content
-
-    end = content.find(">", begin + 1)
-    if end == -1:
-        return content
-
-    new_content = content[0 : begin] + content[end + 1 : ]
-    return strip_html(content=new_content)
-
-def get_div(content, content_id):
-
-    # Try to find the identifier
-    id_pos = content.find(content_id)
-    if id_pos == -1:
-        pdb.set_trace()
-        return []
-
-    # Now try to find the beginning of the div containing it
-    begin = content.rfind("<div", 0, id_pos)
-    if begin == -1:
-        pdb.set_trace()
-        return []
-
-    # Find end of <div tag
-    content_begin = content.find(">", begin + 4)
-    if content_begin == -1:
-        pdb.set_trace()
-        return []
-
-    # Finally, try to find closing </div>
-    end = content.find("</div>", content_begin + 1)
-    if end == -1:
-        pdb.set_trace()
-        return []
-
-    sub_string = content[content_begin + 1 : end]
-    return [ sub_string ]
-
-def do_export_faq_pair(faq_pair):
-
-    target_id = faq_pair['target_id']
-
-    url = "{}/rest/translation/paragraph/{}".format(server, target_id)
-
-    response = requests.get(url + "?_format=json")
-    if response.status_code != 200:
-        report_err("url {} got status code {}".format(url, response.status_code), response.status_code)
-        return;
-
-    json = response.json()
-    content = json[0]['bp_accordion_section']
-
-    question = get_div(content=content, content_id="field--name-bp-accordion-section-title")
-    answer = get_div(content=content, content_id="field--name-bp-text")
-
-    if not question:
-        # Somehow the question could not be parsed - this should never happen.
-        pdb.set_trace()
-
-    question = strip_html(content=question[0])
-
-    if len(answer) == 1:
-        answer = strip_html(content=answer[0])
-    else:
-        # We got unexpected # of answers:  try to handle this
-        pdb.set_trace()
-
-    return { "question": question, "answer": answer }
-
-def do_export(url):
-
-    auth_values = tuple(settings.CREDENTIALS['DETROITMI'].values())
-
-    url = "{}{}".format(server, url)
-
-    response = requests.get(url + "?_format=json", auth=HTTPBasicAuth(*auth_values))
-    if response.status_code != 200:
-        report_err("url {} got status code {}".format(url, response.status_code), response.status_code)
-        return;
-
-    data = response.json()
-
-    if not needs_translation(data):
-        return
-
-    if in_review(data):
-        report_err("url {} is still in REVIEW".format(url), "REVIEW status")
-        return
-
-    # Handle any faq pairs
-    for idx, faq_pair in enumerate(data.get('field_faq_pair', [])):
-
-        if faq_pair['target_type'] == 'paragraph':
-
-            parsed_faq_pair = do_export_faq_pair(faq_pair=faq_pair)
-            data['field_faq_pair'][idx].update(parsed_faq_pair)
-
-        else:
+        if not content:
             pdb.set_trace()
+            return True
 
-    print("url: " + url)
+        changed = content.get('changed')
+        if not changed:
+            pdb.set_trace()
+            return True
 
-    print(json.dumps(data))
-    export_cnt[url] = True
-    print("")
+        tmp = changed[0]['value']
+        date_changed = datetime.strptime(tmp[0 : 10], '%Y-%m-%d').date()
+
+        for key in [ "field_dept_translation_date", "field_gov_translation_date" ]:
+
+            if content.get(key):
+
+                tmp = content[key][0]['value']
+                translation_date = datetime.strptime(tmp, '%Y-%m-%d').date()
+                if translation_date < date_changed:
+                    return True
+                else:
+
+                    # pdb.set_trace()
+
+                    return False
+
+        return True
+
+    @staticmethod
+    def get_div(content, content_id, start=0):
+
+        # Try to find the identifier
+        id_pos = content.find(content_id, start)
+        if id_pos == -1:
+            return [None, None]
+
+        # Now try to find the beginning of the div containing it
+        begin = content.rfind("<div", 0, id_pos)
+        if begin == -1:
+            pdb.set_trace()
+            return [None, None]
+
+        # Find end of <div tag
+        content_begin = content.find(">", begin + 4)
+        if content_begin == -1:
+            pdb.set_trace()
+            return [None, None]
+
+        # Finally, try to find closing </div>
+        end = content.find("</div>", content_begin + 1)
+        if end == -1:
+            pdb.set_trace()
+            return [None, None]
+
+        sub_string = content[content_begin + 1 : end]
+        return [ sub_string, end ]
+
+    @staticmethod
+    def export_faq_pair(faq_pair):
+
+        target_id = faq_pair['target_id']
+
+        url = "{}/rest/translation/paragraph/{}".format(server, target_id)
+
+        response = requests.get(url + "?_format=json")
+        if response.status_code != 200:
+            ContentExporter.report_err("url {} got status code {}".format(url, response.status_code), response.status_code)
+            return;
+
+        json = response.json()
+        content = json[0]['bp_accordion_section']
+
+        faq_pairs = []
+        start = 0
+
+        while True:
+
+            question, start = ContentExporter.get_div(content=content, content_id="field--name-bp-accordion-section-title", start=start)
+            answer, start = ContentExporter.get_div(content=content, content_id="field--name-bp-text", start=start)
+
+            if not question or not answer:
+                return faq_pairs
+
+            faq_pairs.append( { "question": question, "answer": answer } )
+
+    @staticmethod
+    def handle_howdoi(url, data):
+
+        if "how-do-i" not in url.lower():
+            return None
+
+        for key in ['field_department']:
+
+            related = data.get(key)
+            if related:
+                tmp_url = related[0].get('url', '')
+                if tmp_url:
+                    return tmp_url
+
+        # we did not find good related content, so don't try to retrieve any
+        return None
+
+    @staticmethod
+    def handle_faq(data):
+
+        field_faq_refer = data.get('field_faq_refer')
+        if field_faq_refer:
+            url = field_faq_refer[0].get('url')
+            if url:
+                urls.append(url)
+
+    @staticmethod
+    def get_data(url):
+
+        auth_values = tuple(settings.CREDENTIALS['DETROITMI'].values())
+
+        url = "{}{}".format(server, url)
+
+        response = requests.get(url + "?_format=json", auth=HTTPBasicAuth(*auth_values))
+        if response.status_code != 200:
+            ContentExporter.report_err("url {} got status code {}".format(url, response.status_code), response.status_code)
+            return [None, None];
+
+        url = response.url
+
+        # Have we already exported this content?
+        if ContentExporter.urls_exported.get(response.url):
+            ContentExporter.report_err("url {} has already been exported".format(url), "Duplicate URL")
+            return [None, None];
+
+        data = response.json()
+        tmp_url = ContentExporter.handle_howdoi(url, data)
+        ContentExporter.handle_faq(data)
+        if tmp_url:
+            return ContentExporter.get_data(url=tmp_url)
+        else:
+            return url, data
+
+    @staticmethod
+    def cleanup_url(url):
+
+        pos = url.find('?')
+        if pos > 0:
+            url = url[0 : pos]
+        return url
+
+    @staticmethod
+    def do_export(url):
+
+        url, data = ContentExporter.get_data(url=url)
+        if not url:
+            return
+
+        if not ContentExporter.needs_translation(data):
+            ContentExporter.report_err("url {} does not need translation".format(url), "No translation needed")
+            return
+
+        if ContentExporter.in_review(data):
+            ContentExporter.report_err("url {} is still in REVIEW".format(url), "REVIEW status")
+            return
+
+        # Handle any faq pairs
+        for idx, faq_pair in enumerate(data.get('field_faq_pair', [])):
+
+            if faq_pair['target_type'] == 'paragraph':
+
+                parsed_faq_pairs = ContentExporter.export_faq_pair(faq_pair=faq_pair)
+                data['field_faq_pair'][idx]['content'] = parsed_faq_pairs
+
+            else:
+                pdb.set_trace()
+
+        print("url: " + ContentExporter.cleanup_url(url))
+
+        has_some_required = False
+        required_keys = set(data.keys()).intersection(["description", "summary", "field_faq_pair"])
+        for key in required_keys:
+
+            if data.get(key):
+                has_some_required = True
+
+        if not has_some_required:
+            ContentExporter.report_err("url {} was missing required data".format(url), "Missing required data")
+            return 
+
+        print(json.dumps(data))
+        ContentExporter.urls_exported[url] = True
+        print("")
 
 
 if __name__ == '__main__':
 
     if len(sys.argv) == 2:
-        output_errs = sys.argv[1] == '--debug=true'
+        ContentExporter.output_errs = sys.argv[1] == '--debug=true'
 
     os.environ['DJANGO_SETTINGS_MODULE'] = 'django_apps.settings'
     django.setup()
 
     for url in urls:
 
-        do_export(url=url)
+        ContentExporter.do_export(url=url)
 
-    report_err_cnt()
+    ContentExporter.report_err_cnt()
