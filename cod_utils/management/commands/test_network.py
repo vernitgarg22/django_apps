65790546d919d5f8a5cb2cde647d7af4b39e8364
karlkaebnick <karl.kaebnick@gmail.com>
2018-07-19 14:37:16 -0400

    Add simple script to check for server problems

---
+++ cod_utils/management/commands/test_network.py
@@ -0,0 +1,46 @@
+import requests
+
+from django.core.management.base import BaseCommand, CommandError
+
+from cod_utils.messaging import SlackMsgHandler
+
+
+class Command(BaseCommand):
+    help = """Use this to look for network problems"""
+
+    SERVERS = { 
+        "apis.detroitmi.gov": "10.208.37.172",
+    }
+
+    @staticmethod
+    def try_connect(server):
+
+        try:
+            response = requests.get("http://" + server)
+            return response.ok
+        except:
+            return False
+
+    def handle(self, *args, **options):
+
+        domain_errors = []
+        server_errors = []
+
+        for domain, internal_ip in Command.SERVERS.items():
+
+            if not Command.try_connect(domain):
+
+                domain_errors.append(domain)
+
+                if not Command.try_connect(internal_ip):
+
+                    server_errors.append(internal_ip)
+
+        error_msg = ""
+        if domain_errors:
+            error_msg = "The following domains are down: {}".format(domain_errors)
+        if server_errors:
+            error_msg = error_msg + "\nThe following servers are down: {}".format(server_errors)
+
+        if error_msg:
+            SlackMsgHandler().send(message=error_msg)
