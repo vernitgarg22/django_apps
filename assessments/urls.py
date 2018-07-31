4b0b9211d2d8f257c3b3a3caefb30147ec4d67ac
karlkaebnick <karl.kaebnick@gmail.com>
2018-07-31 14:34:46 -0400

    assessments:  clean up urls

--- assessments/urls.py
+++ assessments/urls.py
@@ -9,15 +9,12 @@ urlpatterns = [
     url(r'^address/(?P<address>[-\w\_\.\ ]+)/recent/$', views.get_sales_property_address_recent),
     url(r'^address/(?P<address>[-\w\_\.\ ]+)/recent/years/(?P<years_back>[0-9]+)/$', views.get_sales_property_address),
 
-    # url(r'^(?P<pnum>[\.\w-]+)/$', views.get_sales_property),
-    # url(r'^(?P<pnum>[-\w]+)/$', views.get_sales_property),
     url(r'^(?P<pnum>[-\w\_\.]+)/$', views.get_sales_property),
     url(r'^(?P<pnum>[-\w\_\.]+)/recent/$', views.get_sales_property_recent),
     url(r'^(?P<pnum>[-\w\_\.]+)/recent/years/(?P<years_back>[0-9]+)/$', views.get_sales_property),
 
-    # REVIEW TODO for now disable images
-    # url(r'^(?P<pnum>[-\w\_\.]+)/images/$', views.get_images),
-    # url(r'^image/(?P<id>[\d]+)/$', views.get_image),
+    url(r'^(?P<pnum>[-\w\_\.]+)/images/$', views.get_images),
+    url(r'^image/(?P<id>[\d]+)/$', views.get_image),
 
     # url(r'^parcel/owner_groups/$', views.get_parcel_ownership_groups),
     url(r'^parcel/owner_groups/$', views.ParcelOwnershipGroupsView.as_view(), name='owner_groups'),
