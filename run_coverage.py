ae76769be8f1dbd8dc55df10d9be19b83e148851
karlkaebnick <karl.kaebnick@gmail.com>
2018-08-01 17:33:24 -0400

    Testing:  tweak code coverage script for ubuntu

--- run_coverage.py
+++ run_coverage.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import subprocess
 
+def run_cmd(cmd, errmsg, fallback=None):
 
-def run_cmd(cmd, errmsg):
-
-    return_value = subprocess.run(cmd, shell=True)
-    if return_value.returncode != 0:
+    return_value = subprocess.run(cmd, shell=True, stderr=subprocess.DEVNULL)
+    if return_value.returncode == 0:
+        return True
+    if fallback:
+        return run_cmd(cmd=fallback, errmsg=errmsg)
+    else:
         raise Exception(errmsg)
 
-run_cmd("coverage run run_tests.py", "One or more tests failed")
-run_cmd("coverage html", "Error generating coverage html")
-run_cmd("start htmlcov/index.html", "Error opening coverage report")
+run_cmd(cmd="coverage run run_tests.py", errmsg="One or more tests failed")
+run_cmd(cmd="coverage html", errmsg="Error generating coverage html")
+run_cmd(cmd="start htmlcov/index.html", fallback="coverage report", errmsg="Error opening coverage report")
