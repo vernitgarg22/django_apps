e6999a09e8527674a5996c3c6cd551a0e19eebe7
karlkaebnick <karl.kaebnick@gmail.com>
2019-04-17 18:29:36 -0400

    Get mysql connection working

--- website_data/views.py
+++ website_data/views.py
@@ -11,7 +11,7 @@ from rest_framework import status
 
 from waste_notifier.models import Subscriber
 
-# from website_data.website_db_engine import WebsiteDBEngine
+from website_data.website_db_engine import WebsiteDBEngine
 
 from cod_utils.util import date_json
 
@@ -22,42 +22,39 @@ def parse_date(val):
     dt = timezone.make_aware(dt)
     return dt.date()
 
-
 def get_page_count(start=None, end=None):
 
-    return 0
+    node_count = 0
+    term_count = 0
+    engine = WebsiteDBEngine('detroitmi.prod')
+    try:
+        engine.start()
+        if start and end:
+            start_seconds = int(time.mktime(start.timetuple()))
+            end_seconds = int(time.mktime(end.timetuple()))
+            results = engine.get(
+                "select count(distinct n.nid) as 'count' "
+                "from node n join node_revision nr on n.nid = nr.nid "
+                "where type in :types and revision_timestamp between :start and :end ;",
+                types=('faq', 'how_do_i', 'page', 'story', 'web_apps'), start=start_seconds, end=end_seconds)
+        else:
+            results = engine.get(
+                    "select count(distinct n.nid) as 'count' "
+                    "from node n join node_revision nr on n.nid = nr.nid "
+                    "where type in :types ;",
+                    types=('faq', 'how_do_i', 'page', 'story', 'web_apps'))
 
-    # node_count = 0
-    # term_count = 0
-    # engine = WebsiteDBEngine('detroitmi.prod')
-    # try:
-    #     engine.start()
-    #     if start and end:
-    #         start_seconds = int(time.mktime(start.timetuple()))
-    #         end_seconds = int(time.mktime(end.timetuple()))
-    #         results = engine.get(
-    #             "select count(distinct n.nid) as 'count' "
-    #             "from node n join node_revision nr on n.nid = nr.nid "
-    #             "where type in :types and revision_timestamp between :start and :end ;",
-    #             types=('faq', 'how_do_i', 'page', 'story', 'web_apps'), start=start_seconds, end=end_seconds)
-    #     else:
-    #         results = engine.get(
-    #                 "select count(distinct n.nid) as 'count' "
-    #                 "from node n join node_revision nr on n.nid = nr.nid "
-    #                 "where type in :types ;",
-    #                 types=('faq', 'how_do_i', 'page', 'story', 'web_apps'))
+        node_count = results[0]['count']
 
-    #     node_count = results[0]['count']
+        results = engine.get("select count(distinct tid) as 'count' from taxonomy_term_data where langcode = :lang ;", lang='en')
+        term_count = results[0]['count']
 
-    #     results = engine.get("select count(distinct tid) as 'count' from taxonomy_term_data where langcode = :lang ;", lang='en')
-    #     term_count = results[0]['count']
+    except:
+        return 0
 
-    # except:
-    #     return 0
-
-    # finally:
-    #     engine.stop()
-    #     return node_count + term_count
+    finally:
+        engine.stop()
+        return node_count + term_count
 
 @api_view(['GET'])
 def get_new_content(request, start=None, end=None, format=None):
