4c57db557188d0db65bdbd3955ced57d21d50539
Karl Kaebnick <karl.kaebnick@gmail.com>
2019-06-11 14:22:01 -0400

    Waste Notifier:  script to clean up addresses

---
+++ update_subscriber_responses.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import csv
+from waste_notifier.models import Subscriber
+from waste_notifier.util import geocode_address, get_waste_area_ids
+
+
+import pdb
+
+
+pdb.set_trace()
+    
+
+filename = "subscriber_responses.csv"
+
+with open(filename, newline='') as csvfile:
+
+    # From,To,Body,Status,SentDate,ApiVersion,NumSegments,ErrorCode,AccountSid,Sid,Direction,Price,PriceUnit
+    reader = csv.DictReader(csvfile)
+
+    for row in reader:
+
+        phone_number = row['From']
+        subscriber = Subscriber.objects.get(phone_number = phone_number)
+
+        # What if subscriber not found?
+        if not subscriber:
+
+
+            pdb.set_trace()
+
+
+            continue
+
+        # Don't overwrite existing address
+        if subscriber.address:
+
+            continue
+
+        # Geocode the address.
+        street_address = row['Body']
+        location, address = geocode_address(street_address=street_address)
+
+        # What if no location was found?
+        if not location:
+            print(f"Address '{street_address}' could not be geocoded")
+            sys.stdout.flush()
+
+            continue
+
+        # Lookup up the waste area id.
+        waste_area_ids = get_waste_area_ids(location=location)
+        if not waste_area_ids:
+
+
+            pdb.set_trace()
+
+
+            print(f"ERROR:  No waste area ids found for address '{street_address}'")
+            sys.stdout.flush()
+            return
+
+
+        if type(waste_area_ids) == list:
+            waste_area_ids = ''.join( [ str(num) + ',' for num in waste_area_ids ] )
+
+        # Update subscriber waste area id and save
+        subscriber.waste_area_ids = waste_area_ids
+        subscriber.save(force_update=True)
